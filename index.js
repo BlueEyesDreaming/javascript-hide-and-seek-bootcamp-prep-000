518c78cab39e2d57a33e9e1b154c1a1de61d114b
Ashton Reed <dreamergirl411@gmail.com>
2018-01-12 21:56:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function increaseRankBy(n) {
   for (let i=0; i < lis.length; i++) {
 
 
-    const int = parseInt(lis[i], 10)
-    lis[i].innerHTML = (int[i] + n).toString()
+    // const int = parseInt(lis[i], 10)
+    // lis[i].innerHTML = (int[i] + n).toString()
   }
 }
