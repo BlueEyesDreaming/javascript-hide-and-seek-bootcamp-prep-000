73e0e31b498cf841c2cc0b5ef8739a4b9645af76
Ashton Reed <dreamergirl411@gmail.com>
2018-01-12 21:32:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+ function getFirstSelector(selector) {
+   return document.querySelector(selector)
+ }
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function deepestChild() {
+  return document.getElementById('grand-node').querySelector('div div div div div')
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i=0; i <lis.length; i++) {
+    const int = parseInt(lis[i], 10)
+    lis[i].innerHTML = (i + n).toString()
+  }
+  return lis
+}
